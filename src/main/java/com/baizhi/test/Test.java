ea57ab8983eb9a487a701ad45a1b579949b82c6a
bo279759515 <44286786+asasaasas12@users.noreply.github.com>
2018-10-19 21:12:00 +0800

    Update Test.java

--- src/main/java/com/baizhi/test/Test.java
+++ src/main/java/com/baizhi/test/Test.java
@@ -3,5 +3,7 @@ package com.baizhi.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("wq j g wh dd wtgk");
+        System.out.println("终于他妈的好了！");
+
     }
 }
